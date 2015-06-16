ca264187d00853746b8ac5a437439b15454d0bac
Matthew Smart <mdsmart36@gmail.com>
2015-06-15 21:27:39 -0500

    begin authentication

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,38 @@
 app.controller('homeController', ['$scope', function($scope) {
   $scope.welcome = "Welcome to Track Coach / Login and Registration";
+  $scope.showRegister = false;
+  $scope.showLogin = true;
+  $scope.user = {};
+
+  var ref = new Firebase("https://track-coach.firebaseio.com");
+
+  
+
+  $scope.showLoginBtn = function(){
+    $scope.showRegister = false;
+    $scope.showLogin = true;    
+  }
+
+  $scope.showRegisterBtn = function(){
+    $scope.showRegister = true;
+    $scope.showLogin = false;    
+  }
+
+  $scope.login = function() {
+
+  }
+
+  $scope.register = function() {
+    ref.createUser({
+      email    : $scope.user.email,
+      password : $scope.user.password
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+  }
+
 }]);
